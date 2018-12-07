c490d215b156a1bda9b50f32e31a96161c471d81
sanyarelan25 <43932833+sanyarelan25@users.noreply.github.com>
2018-12-07 09:07:38 -0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -24,4 +24,37 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/store/:id", async (req, res) => {
+  try {
+    const store = await adminData.getStoreById(req.params.id);
+    res.json(store);
+  } catch (e) {
+    res.status(404).json({ error: "Store not found by id" });
+  }
+});
+
+router.post("/store", async (req, res) => {
+  const newlyAddedStoreData = req.body;
+  try {
+    const { storeName, address, phoneNo, email } = newlyAddedStoreData;
+    const newStore = await adminData.addStore(storeName, address, phoneNo, email);
+
+    res.json(newStore);
+  } catch (e) {
+    res.status(500).json({ error: "Unable to add new Store" });
+  }
+});
+
+router.delete("/store/:id", async (req, res) => {
+  try {
+    await adminData.getStoreById(req.params.id);
+  } catch (e) {
+    res.status(404).json({ error: "Store not found" });
+  }
+  try {
+    await adminData.removeStore(req.params.id);
+  } catch (e) {
+    res.status(500).json({ error: "Cannot delete Store by id" });
+  }
+});
 module.exports = router;
