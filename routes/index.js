5953c37c4da4e14f09a826f024aae971089eb6bb
sanyarelan25 <43932833+sanyarelan25@users.noreply.github.com>
2018-12-07 08:28:11 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const constructorMethod = app => {
   app.use("/users", userRoutes);
   app.use("/stores", storeRoutes);
   app.use("/prod", prodRoutes);
+  app.use("/admin", adminRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
   });
