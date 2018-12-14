ac6dd632e15a0ac9d31d59be73a5908e1ed1a388
Sanya Relan <relans1093@gmail.com>
2018-12-14 15:53:09 -0500

    changes in app.js

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const express = require("express");
-const app = express();
-const configRoutes = require("./routes");
+/* Importing all the neccessary modules */
+const express = require('express');
 const bodyParser = require("body-parser");
-const exphbs=require("express-handlebars");
-const static = express.static(__dirname+"/prod");
-app.use("/public",static);
-app.use(bodyParser.urlencoded());
+const exphbs = require("express-handlebars");
 
-app.engine("handlebars",exphbs({defaultLayout:"main"}));
-app.set("view engine","handlebars");
+/* Starting Express Server */
+const app = express();
 
+/* Body Parser Configuration */
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Static Page Configuration */
+const static = express.static(__dirname + '/public');
+app.use("/public", static);
+
+/* Handlebars Configuration */
+app.engine('handlebars', exphbs({ defaultLayout:'main' }));
+app.set('view engine', 'handlebars');
+
+/* Routing Configuration */
+const configRoutes = require("./routes");
 configRoutes(app);
 
+/* Server is running on port 3000 */
 app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:3000");
+    console.log("We've now got a server");
+    console.log("Your routes will be running on http://localhost:3000");
 });
