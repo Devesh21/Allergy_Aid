b9c1f0492eaba0d7a71bbf55aac255840af92849
Sanya Relan <relans1093@gmail.com>
2018-12-14 16:18:10 -0500

    complete new

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const userRoutes = require("./users");
 const prodRoutes = require("./prod");
 const storeRoutes = require("./stores");
 const adminRoutes = require("./admin");
-
+const homeRoutes=require("./home");
 
 const constructorMethod = app => {
   app.use("/users", userRoutes);
   app.use("/stores", storeRoutes);
   app.use("/prod", prodRoutes);
   app.use("/admin", adminRoutes);
+  app.use("/", homeRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
   });
