268d6d04a330ad40b8d2824fe03309ba0b761ad4
Devesh Naik <dev.naik2102@gmail.com>
2018-12-15 04:39:49 -0500

    routes

--- routes/home.js
+++ routes/home.js
@@ -10,20 +10,12 @@ router.get('/index/about', (req, res) => {
 });
 
 
-router.get('/index/Usersignup', (req,res) => {
-    res.render('users/signup');
-});
-
-router.get('/index/login', (req,res) => {
-    res.render('users/login');
-});
-
 router.get('/user/signup', (req,res) => {
     res.render('users/signup');
 });
 
 router.get('/user/login', (req,res) => {
     res.render('users/login');
-})
+});
 
 module.exports = router;
